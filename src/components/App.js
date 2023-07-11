b9654610903f7f8fab52db0a12fcbe79f27c0425
Mohit Kamdi <mohitkamdi317@gmail.com>
2023-07-11 12:49:46 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+ 
+  const[ct,updatedCt] = useState(0);
+
+  const counter = () =>{
+    updatedCt(ct + 1)
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked {ct} times</p>
+        <button onClick={counter}>Click</button>
     </div>
   )
 }
